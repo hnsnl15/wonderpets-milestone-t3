7d2d5d7f6cb9fd70dab7cf4b280ec16c42c12e1a
henson <hensonlao@gmail.com>
2023-04-06 00:44:41 +0800

    Established connection to Google Sheet API

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,11 @@ module com.wonderpets.payroll_gui_v2 {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires net.synedra.validatorfx;
+    requires google.api.client;
+    requires google.oauth.client;
+    requires google.http.client;
+    requires google.http.client.jackson2;
+    requires google.api.services.sheets.v4.rev581;
 
     opens com.wonderpets.payroll_gui_v2 to javafx.fxml;
     exports com.wonderpets.payroll_gui_v2;
