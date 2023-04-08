001fed26174aff2a861876ed12bda3b1043adffd
henson <hensonlao@gmail.com>
2023-04-09 01:59:35 +0800

    Minor changes

--- src/main/java/com/wonderpets/payroll_gui_v2/controller/employee/EmployeeApplication.java
+++ src/main/java/com/wonderpets/payroll_gui_v2/controller/employee/EmployeeApplication.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 
 public class EmployeeApplication extends Application {
 
-    public static void main(String[] args) {
+    public static void main(String... args) {
         launch();
     }
 
