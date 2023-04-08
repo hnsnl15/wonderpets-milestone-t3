cf32bb27f358a575f1402b7baa080f43f4ff39a8
henson <hensonlao@gmail.com>
2023-04-08 21:53:47 +0800

    Some minor changes

--- src/main/java/com/wonderpets/payroll_gui_v2/controller/employee/EmployeeApplication.java
+++ src/main/java/com/wonderpets/payroll_gui_v2/controller/employee/EmployeeApplication.java
@@ -17,7 +17,7 @@ public class EmployeeApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(EmployeeApplication.class.getResource("/com/wonderpets/payroll_gui_v2/employee-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1080, 568);
-        stage.setTitle("Employee");
+        stage.setTitle("MotorPH Payroll Management System");
         stage.setScene(scene);
         stage.show();
     }
