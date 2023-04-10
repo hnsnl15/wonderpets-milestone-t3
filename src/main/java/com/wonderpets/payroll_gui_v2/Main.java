f58463520191b3549df52e21a423b3d9f2b1510b
henson <hensonlao@gmail.com>
2023-04-10 12:34:19 +0800

    Minor changes

--- src/main/java/com/wonderpets/payroll_gui_v2/Main.java
+++ src/main/java/com/wonderpets/payroll_gui_v2/Main.java
@@ -3,7 +3,7 @@ package com.wonderpets.payroll_gui_v2;
 import com.wonderpets.payroll_gui_v2.controller.employee.EmployeeApplication;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         EmployeeApplication.main();
     }
 }
